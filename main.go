159e4cef27e0585fca20818b266715f4ba63d05a
reinhardbuyabo <buyabor@gmail.com>
2023-08-02 19:24:26 +0300

    admin.html from a good pgrma

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"sync"
 
 	"github.com/go-chi/chi"
 )
 
 type apiConfig struct {
 	fileServerHits int
+	mutex          sync.Mutex
 }
 
 func main() {
 	apiCfg := apiConfig{}
 	r := chi.NewRouter()
+
 	// Step 1:
 	// mux := http.NewServeMux()
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	r.Handle("/app", fileServerHandler) // You'll need to .Handle the fileserver handler twice, once for the /app/* path and once for the /app path (without the trailing slash).
-	r.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricInc(http.FileServer(http.Dir(".")))))
-	// mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricInc(http.FileServer(http.Dir("."))))) // 3. Wrapping the FileServer with the MiddleWare we just wrote
 
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	r.Handle("/app", apiCfg.middlewareMetricInc(fileServerHandler)) // You'll need to .Handle the fileserver handler twice, once for the /app/* path and once for the /app path (without the trailing slash).
+	r.Handle("/app/*", apiCfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	// mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricInc(http.FileServer(http.Dir("."))))) // 3. Wrapping the FileServer with the MiddleWare we just wrote
+	apiRouter := chi.NewRouter()
+	apiRouter.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(http.StatusText(http.StatusOK)))
@@ -38,14 +42,18 @@ func main() {
 	// 	w.WriteHeader(http.StatusOK)
 	// 	w.Write([]byte(http.StatusText(http.StatusOK)))
 	// })
+	adminRouter := chi.NewRouter()
 
-	r.Get("/metrics", apiCfg.metricsHandler)
+	adminRouter.Get("/metrics", apiCfg.metricsHandler)
 
 	// r.HandleFunc("/metrics", apiCfg.metricsHandler)
 
 	// mux.HandleFunc("/metrics", apiCfg.metricsHandler)
 	// Step 2:
+
 	corsMux := middlewareCors(r)
+	r.Mount("/api", apiRouter)
+	r.Mount("/admin", adminRouter)
 
 	// Step 3:
 	server := &http.Server{
@@ -80,8 +88,32 @@ func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler { // 2
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	// writes the number of requests that have been counted as plain text
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.mutex.Lock()
+	visits := cfg.fileServerHits
+	cfg.mutex.Unlock()
 
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+	// Parse the HTML template from the file.
+	tmpl, err := template.ParseFiles("admin.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response Content-Type header to "text/html".
+	w.Header().Set("Content-Type", "text/html")
+
+	// Execute the template with the PageData and write the output to the response.
+	data := struct {
+		Title  string
+		Visits int
+	}{
+		Title:  "Welcome, Chirpy Admin",
+		Visits: visits,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
